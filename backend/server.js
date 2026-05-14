cd5f77b1d9b35f6af215212d85805642af7dd465
SHARMA1525 <saipraneethsharma57@gmail.com>
2026-05-14 19:25:52 +0530

    fix: allow vercel origin in cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+  origin: true, // Allow all origins in development/production for easy debugging
   credentials: true,
 }));
 
